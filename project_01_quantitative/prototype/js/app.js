cc06c0bef40c0b35f950e76fe88d35df5289a137
shmanzar <manzs324@newschool.edu>
2020-09-24 16:34:53 +0000

    added new prototype code | scrape succesful | parsing WIP

--- project_01_quantitative/prototype/js/app.js
+++ project_01_quantitative/prototype/js/app.js
@@ -1,3 +1,25 @@
-/// <reference path="../node_modules/@types/p5/global.d.ts"/>
-/// <reference path="../node_modules/@types/d3/index.d.ts"/>
+// Smithsonian API example code
+// check full API documentation here: https://edan.si.edu/openaccess/apidocs/
 
+
+// put your API key here;
+const apiKey = "YZ7GP6NjkhYNhJDzyHaCv88g89m0Ip0lryIw36LZ";
+
+// Access to individual objects by ID
+const objectBaseURL = "https://api.si.edu/openaccess/api/v1.0/content/";
+
+//fetches content based on id of an object.
+function fetchContentDataById(id) {
+  let url = objectBaseURL + id + "?api_key=" + apiKey;
+  window
+    .fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log("Here's the content data of the specified object:", data.response);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+}
+// fetchContentDataById("edanmdm:NMAI_270941");
+fetchContentDataById("edanmdm:nmaahc_2015.97.42");
---
+++ project_01_quantitative/prototype/js/scraper.js
@@ -0,0 +1,34 @@
+// const puppeteer = require('puppeteer');
+var cheerio = require('cheerio');
+var request = require('request');
+var fs = require('fs');
+
+
+
+var link_array = [];
+
+for (let i = 1; i <= 27; i++) {
+    if (i < 10) {
+        link_array.push('https://transcription.si.edu/view/7955/NMAAHC-2015_97_42_00' + i);
+
+    }
+    if (i >= 10) {
+        link_array.push('https://transcription.si.edu/view/7955/NMAAHC-2015_97_42_0' + i);
+
+    }
+}
+
+function getData(link) {
+    request(link, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(response, body)
+            let filename = link.match(/_\d{3}$/g);
+            fs.writeFileSync('../data/' + filename + '.txt', body);
+            console.log("File downloaded!");
+        }
+        else { console.log("Request failed!") }
+    });
+}
+
+link_array.forEach(link => getData(link));
+
