bd27047b57f45781facbe165a652d48e64b5ab17
shmanzar <email@smanzar.com>
2020-12-08 01:37:26 -0500

    fixed column1 and hover

---
+++ project_03_interactive/final/js/app.js
@@ -0,0 +1,5 @@
+let button = document.getElementById("test");
+button.addEventListener("click", function () {
+  console.log("test");
+  document.querySelector(".const-text").classList.toggle("const-text-green");
+});
